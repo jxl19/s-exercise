1c2b23ce2cf56a32153363191ed3d6f0c19b8fc1
jylei <jlei91@gmail.com>
2018-05-26 09:13:56 -0700

    using form components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import FormContainer from './FormContainer';
+// import './App.css';
+import './main.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <FormContainer />
       </div>
     );
   }
