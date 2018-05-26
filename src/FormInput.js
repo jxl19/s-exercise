9a61eade83a9fecf23a9b3dc2b0e1c668ae226c8
jylei <jlei91@gmail.com>
2018-05-26 09:53:56 -0700

    input validation

--- src/FormInput.js
+++ src/FormInput.js
@@ -4,12 +4,14 @@ const FormInput = (props) => (
     <div className="form-group">
         <label htmlFor="form-label">{props.title}</label>
         <input 
-            className="form-control"
+            className={props.className}
+            onBlur={props.onBlur}
             name={props.name}
             type={props.inputType}
             value={props.content}
             onChange={props.controlFunction}
             placeholder={props.placeholder}/>
+            <span class="error">test</span>
     </div>
 )
 
