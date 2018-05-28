858873d1f8f619edfa8c03d6538a6acc41509683
jylei <jlei91@gmail.com>
2018-05-28 10:32:51 -0700

    fix cb background

--- src/FormCheckbox.js
+++ src/FormCheckbox.js
@@ -1,21 +1,20 @@
 import React from 'react';
+import './FormCheckbox.css';
 const FormCheckBox = (props) => (
 
   <div className="checkbox">
-    <span className="conditions">{props.name}</span>
+    <span className="conditions">{props.title}</span>
     <label>
       <input
         type="checkbox"
         className="checkbox-input"
         checked={props.checked}
         onChange={props.handleChange}
-        name={props.name}
-        title={props.title}
       />
       <span
         className="checkbox-span"
       />
-      <div className={props.className}>{props.title}<a href={props.href} target="_blank">{props.link}</a></div>
+      <div className={props.className}>{props.content}<a href={props.href} target="_blank">{props.link}</a></div>
     </label>
   </div>
 )
