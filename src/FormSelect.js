786a00f1cfd0e4432567389a702abbd288ad8e5b
jylei <jlei91@gmail.com>
2018-05-28 10:34:21 -0700

    split css

--- src/FormSelect.js
+++ src/FormSelect.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import './FormSelect.css';
 const FormSelect = (props) => (
 
     <div className="btn-group dropup">
         <div className="selection-title">{props.selectionTitle}</div>
         <button type="button" className="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <span className="default-option">{props.selectedOption}</span>
+            <span className={props.className}>{props.optionSelect ? props.optionSelect : props.placeholder}</span>
 </button>
         <div className="dropdown-menu dropdown-menu-right">
             {props.options.map(opt => {
